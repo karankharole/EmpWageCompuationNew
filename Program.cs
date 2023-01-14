0de6900059c33acd907034955d13067735d889a9
karankharole <karankharole2000@gmail.com>
2023-01-14 07:31:15 +0530

    calculating wage using interface

--- Program.cs
+++ Program.cs
@@ -6,18 +6,12 @@ namespace EmpWageCompuationNew
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderObject Airtel = new EmpWageBuilderObject("Airtel", 30, 30, 100);
-            EmpWageBuilderObject TCS = new EmpWageBuilderObject("TCS", 40, 30, 80);
-            EmpWageBuilderObject Wipro = new EmpWageBuilderObject("Wipro", 25, 25, 120);
-            EmpWageBuilderObject Cipla = new EmpWageBuilderObject("Cipla", 23, 28, 111);
-            Airtel.computeEmpWage();
-            Console.WriteLine(Airtel.toString());
-            TCS.computeEmpWage();
-            Console.WriteLine(TCS.toString());
-            Wipro.computeEmpWage();
-            Console.WriteLine(Wipro.toString());
-            Cipla.computeEmpWage();
-            Console.WriteLine(Cipla.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addcompanyEmpWage("Airtel", 30, 30, 100);
+            empWageBuilder.addcompanyEmpWage("TCS", 40, 30, 80);
+            empWageBuilder.addcompanyEmpWage("Wipro", 25, 25, 120);
+            empWageBuilder.addcompanyEmpWage("Cipla", 23, 28, 111);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
