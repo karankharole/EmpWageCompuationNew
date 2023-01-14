f54eacd5da92d1a68a3531e13971e83880c8476c
karankharole <karankharole2000@gmail.com>
2023-01-14 07:22:02 +0530

    calculating wage for total company

---
+++ EmpWageBuilderObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCompuationNew
+{
+    internal class EmpWageBuilderObject
+    {
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void computeEmpWage()
+        {
+            // Variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            // Comutation
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage of " + company + " is :" + totalEmpWage);
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage of " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,48 +1,23 @@
-﻿namespace EmpWageCompuationNew
+﻿using EmpWageCompuationNew;
+
+namespace EmpWageCompuationNew
 {
-    internal class Program
+    public class Program
     {
-
-
-        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2;
-
-        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            // Variables
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days : " + totalWorkingDays + "Emp Hrs : " + empHrs);
-            }
-
-            int totalempwage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage of " + company + " is :" + totalempwage);
-            return totalempwage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("IBM", 30, 30, 100);
-            computeEmpWage("TCS", 40, 30, 80);
-            computeEmpWage("Wipro", 25, 25, 120);
-            computeEmpWage("DXC", 23, 28, 111);
+            EmpWageBuilderObject Airtel = new EmpWageBuilderObject("Airtel", 30, 30, 100);
+            EmpWageBuilderObject TCS = new EmpWageBuilderObject("TCS", 40, 30, 80);
+            EmpWageBuilderObject Wipro = new EmpWageBuilderObject("Wipro", 25, 25, 120);
+            EmpWageBuilderObject Cipla = new EmpWageBuilderObject("Cipla", 23, 28, 111);
+            Airtel.computeEmpWage();
+            Console.WriteLine(Airtel.toString());
+            TCS.computeEmpWage();
+            Console.WriteLine(TCS.toString());
+            Wipro.computeEmpWage();
+            Console.WriteLine(Wipro.toString());
+            Cipla.computeEmpWage();
+            Console.WriteLine(Cipla.toString());
         }
     }
-    
 }
